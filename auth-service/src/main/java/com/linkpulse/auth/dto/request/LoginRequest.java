8b9bfbe238cc12b1c0b628dd5a93baba69932996
codesche <codesche@gmail.com>
2025-09-13 15:38:19 +0900

    feat(auth): 로그인 dto 작성

---
+++ auth-service/src/main/java/com/linkpulse/auth/dto/request/LoginRequest.java
@@ -0,0 +1,21 @@
+package com.linkpulse.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequest {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+
+}
